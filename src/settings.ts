4cc1db0bb6d543b4bcdd04da714bb165f66e7d07
Teagan glenn <teagan.m.glenn@gmail.com>
2020-07-16 20:07:53 -0600

    Homebridge settings

---
+++ src/settings.ts
@@ -0,0 +1,9 @@
+/**
+ * This is the name of the platform that users will use to register the plugin in the HomeBridge config.json
+ */
+export const PLATFORM_NAME = 'CallScheduling';
+
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export const PLUGIN_NAME = 'homebridge-scheduled-callss';
